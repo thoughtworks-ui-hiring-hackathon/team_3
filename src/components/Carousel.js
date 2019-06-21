a8a25c7a53b8867c5765b75b5a98301426a689d6
Mohan Dere <deremohan01@gmail.com>
2019-06-21 17:18:32 +0530

    carousel responsive

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,6 +2,39 @@ import React from 'react';
 import Slider from 'react-slick';
 import isEmpty from 'lodash/isEmpty';
 
+ const settings = {
+  dots: false,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+};
+
 class Carousel extends React.Component {
   render() {
     let { slides } = this.props;
@@ -29,12 +62,7 @@ class Carousel extends React.Component {
         </div>
       );
     });
-    var settings = {
-      dots: true,
-      speed: 500,
-      slidesToShow: 4,
-      slidesToScroll: 4
-    };
+
     return (
       <Slider className="carousel" {...settings}>
         {slidesHtml}
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,12 +5,37 @@ import { MOVIE_CATEGORY_STRING } from '../config/constants.js';
 import { isEmpty } from 'lodash';
 import Slider from 'react-slick';
 
-var carouselSettings = {
+const carouselSettings = {
   dots: false,
   speed: 500,
   slidesToShow: 4,
   slidesToScroll: 4,
-  infinite: false
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 };
 
 class MovieList extends React.PureComponent {
