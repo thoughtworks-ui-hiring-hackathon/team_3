4e88c8ff4bc3ff2a83eed93a7ce008cb5d29af96
Mohan Dere <deremohan01@gmail.com>
2019-06-21 13:00:06 +0530

    in sync

--- src/screens/details/index.js
+++ src/screens/details/index.js
@@ -2,19 +2,37 @@ import React from 'react';
 import {connect} from 'react-redux';
 import Banner from './components/Banner'
 import {fetchMovie} from '../../actions/movie-actions';
-
+import isEmpty from 'lodash/isEmpty'
 
 class Details extends React.PureComponent {
 
   componentDidMount() {
-    this.props.fetchMovie(320288);
+    let { params } = this.props.match
+    this.props.fetchMovie(params.ID);
   }
 
   render() {
+    let { movieReducer } = this.props;
+    let { isFetching, info, error} = movieReducer;
+
+    if (isFetching) {
+      return (
+        <p>Loading...</p>
+      )
+    }
+
+    if (!isEmpty(error)) {
+      return (
+        <p className="alert alert-danger">
+        {JSON.stringify(error)}
+        </p>
+      )
+    }
+
     return (
       <>
         <h1>Movie Details</h1>
-        <Banner />
+        <Banner info={info} />
       </>
     )
   }
