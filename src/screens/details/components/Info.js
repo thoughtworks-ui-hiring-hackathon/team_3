90910a787ab60d95d341a7be66cbe3328f66b438
Mohan Dere <deremohan01@gmail.com>
2019-06-21 13:21:07 +0530

    up

--- src/screens/details/components/Info.js
+++ src/screens/details/components/Info.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
+import isEmpty from 'lodash/isEmpty'
 
 const Info = ({ info }) => {
-  let { title, overview } = info
+  let { title, overview, genres } = info
+
+  genres = isEmpty(genres) ? [] : genres
+  let genresHtml = genres.map((genre) => (
+    <span>{`${genre.name} `}</span>
+  ))
+
   return (
     <div className="movie--info">
       <Row>
@@ -11,7 +18,12 @@ const Info = ({ info }) => {
           <p>{overview}</p>
         </Col>
         <Col sm={6}>
-          <h1>{title}</h1>
+          <section className="movie--card">
+            <div className="">
+              <strong>Genre</strong>
+              <span>{genresHtml}</span>
+            </div>
+          </section>
         </Col>
       </Row>
     </div>
