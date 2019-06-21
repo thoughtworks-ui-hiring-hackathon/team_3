149c7df7c642e029c2684c4396b1caed98e80376
mrgurdeep <mrgurdeep1989@gmail.com>
2019-06-21 16:07:02 +0530

    Merge branches 'home-page' and 'master' of https://github.com/thoughtworks-ui-hiring-hackathon/team_3 into home-page
    # Conflicts:
    #	src/reducers/root-reducer.js

--- src/components/ActorDetails.js
+++ src/components/ActorDetails.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
+import {isEmpty} from 'lodash';
+import {IMG_BASE_URL} from '../consts';
 
 const ActorDetails = (props) => {
+
+    const {actor, filmography} = props;
+
     return (
-        <div className='actor-wrapper'>
-            <Row>
-                <Col sm={6}>
-                    <img className="image" src='' />
-                </Col>
-            </Row>
-        </div>
+        <>
+            {
+                !isEmpty(actor) && <div className='actor-wrapper'>
+                    <Row>
+                        <Col sm={4}>
+                            <img className="image" src={`${IMG_BASE_URL}${actor.profile_path}`}/>
+                        </Col>
+                        <Col sm={8}>
+                            <div className='description-wrapper'>
+                                <span className='meter'>Meter {actor.popularity}</span>
+                                <span>{actor.name}</span>
+                                <span>Date of Birth {actor.birthday}</span>
+                                <span>{actor.biography}</span>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+            }
+
+        </>
     );
 }
 
