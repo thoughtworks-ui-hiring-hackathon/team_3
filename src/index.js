e2744fa959bd4b468beadc052a8df015a1396753
Mohan Dere <deremohan01@gmail.com>
2019-06-21 11:34:43 +0530

    screens

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import configureStore from './store';
 import './scss/_index.css';
 import App from './app';
+import Movie from './screens/movie';
 
 ReactDOM.render(
 	<Provider store={configureStore()}>
 		<Router>
 			<Route path="/" component={App}/>
+			<Route path="/movie/:ID" component={Movie}/>
 		</Router>
 	</Provider>,
 	document.getElementById('root')
